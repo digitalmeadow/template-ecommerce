bf0de3b5b0acc3673a8684345de2f3a51173153c
Laird Kruger <accounts@lairdkruger.com>
2026-01-10 12:40:45 +1000

    Update client.ts

--- frontend/src/sanity/client.ts
+++ frontend/src/sanity/client.ts
@@ -4,5 +4,9 @@ export const sanityClient = new GraphQLClient(
   `https://${import.meta.env.SANITY_PROJECT_ID}.api.sanity.io/v1/graphql/${
     import.meta.env.SANITY_DATASET
   }/default`,
-  {}
+  {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.SANITY_VIEWER_API_TOKEN}`,
+    },
+  }
 );
